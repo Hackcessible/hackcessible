5e311190d6621981ee475532f8ca02c4204cc457
Allie Deford <allied935@gmail.com>
2015-03-22 13:34:32 -0500

    Update bus icon to match on map

--- static/js/busdata.js
+++ static/js/busdata.js
@@ -1,4 +1,13 @@
 function addMarker(map, lat, lon, name) {
+  var image = {
+    url: '../static/img/bus.png',
+    // This marker is 20 pixels wide by 32 pixels tall.
+    scaledSize: new google.maps.Size(25, 25),
+    // The origin for this image is 0,0.
+    origin: new google.maps.Point(0,0),
+    // The anchor for this image is the base of the flagpole at 0,32.
+    anchor: new google.maps.Point(0, 32)
+  };
   var myLatlng=new google.maps.LatLng(lat, lon);
                 // To add the marker to the map, use the 'map' property
 
@@ -6,7 +15,7 @@ function addMarker(map, lat, lon, name) {
       position: myLatlng,
       map: map,
       title: name, 
-      icon: '../static/img/busstop.png'
+      icon: image
   });
 
   marker.setMap(map); 
